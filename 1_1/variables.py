dd35c4f1ca5a624bf0ff7414a5aef98b9ddbc146
dejvi-S <xzajcew@interia.pl>
2020-01-01 17:46:22 +0100

    First steps, variables,'Hello World',print()

---
+++ 1_1/variables.py
@@ -0,0 +1,9 @@
+name = "Dawid"
+age = 20
+language = "Python"
+nick = "dejviS"
+
+print("Hello my name is " + name)
+print("I'm " + age.__str__() + " yeasr old")
+print("I trying programming in " + language)
+print("My pseudonim is " + nick)
