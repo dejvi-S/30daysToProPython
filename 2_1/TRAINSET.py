fb014d605a57edf6aed30da80dcef2665165129f
dejvi-S <xzajcew@interia.pl>
2020-01-03 23:08:14 +0100

    Good idea, but this is for training set without possibility to duplicate the same training word

--- 2_1/TRAINSET.py
+++ 2_1/TRAINSET.py
@@ -1,12 +1,22 @@
-
+def sil(n):
+    f = lambda x: x * f(x-1) if x != 0 else 1
+    return f(n)
 
 numberOfTestCases = input()
 for i in range(int(numberOfTestCases)):
     elements = input()
     words = []
-    duplicateTF = 0
+    maxElem = 0;
+    minElem = 0;
     for x in range(int(elements)):
         temp = input()
         if temp not in words:
             words.append(temp)
+    maxElem = len(words)
+    words = [word[:-2] for word in words]
+    words = list(set(words))
+    minElem = len(words)
+    combin = (sil(maxElem) / pow(2,(maxElem-minElem)))
+    print(combin)
+
 
