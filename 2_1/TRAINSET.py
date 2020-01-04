cc0b75b666c33d90029de7d6785bd9d1c555420c
dejvi-S <xzajcew@interia.pl>
2020-01-04 22:28:20 +0100

    Input function changed, sorted two dictionaries

--- 2_1/TRAINSET.py
+++ 2_1/TRAINSET.py
@@ -1,28 +1,36 @@
-def sil(n):
-    f = lambda x: x * f(x-1) if x != 0 else 1
-    return f(n)
-
+#make functions! clear code
 numberOfTestCases = input()
 for i in range(int(numberOfTestCases)):
     elements = input()
-    words = []
-    maxElem = 0;
-    minElem = 0;
+    wordsTrue = []
+    wordsFalse = []
     for x in range(int(elements)):
         temp = input()
-        words.append(temp)
-    maxElem = len(words)
-    dictWords = dict()
-    for word in words:
-        if word not in dictWords:
-            dictWords[word] = 1
+        if temp[(len(temp)-1)] == str(1):
+            wordsTrue.append(temp)
         else:
-            dictWords[word] += 1
-    print(dictWords)
-    words = [word[:-2] for word in words]
-    words = list(set(words))
-    minElem = len(words)
-    combin = (sil(maxElem) / pow(2,(maxElem-minElem)))
-    print(combin)
+            wordsFalse.append(temp)
+    wordsTrue = [wordT[:-2] for wordT in wordsTrue]
+    wordsFalse = [wordF[:-2] for wordF in wordsFalse]
+    dictTrue = dict()
+    dictFalse= dict()
+    for word in wordsTrue:
+        if word not in dictTrue:
+            dictTrue[word] = 1
+        else:
+            dictTrue[word] += 1
+    for word in wordsFalse:
+        if word not in dictFalse:
+            dictFalse[word] = 1
+        else:
+            dictFalse[word] += 1
+    print(wordsTrue)
+    print(wordsFalse)
+    del wordsTrue
+    del wordsFalse
+    print(dictFalse)
+    print(dictTrue)
+    for word in
+
 
 
