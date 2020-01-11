3ba1b186777c5c3332f806d89404db50c2c3e699
dejvi-S <xzajcew@interia.pl>
2020-01-12 00:06:54 +0100

    BRKBKS Solved 50% points

--- 2_1/BRKBKS.py
+++ 2_1/BRKBKS.py
@@ -1,6 +1,35 @@
 numberTest = int(input())
+
+
+def maxbricks(power, brick):
+    summary = 0
+    counter = 0
+    for i in range(len(brick)):
+        tempo = summary + brick[i]
+        if power >= tempo:
+            summary = summary + brick[i]
+            counter += 1
+    return int(counter)
+
+
 for x in range(numberTest):
     bricks = input().split(" ")
-    bricks = list(map(int,bricks))
+    bricks = list(map(int, bricks))
     strength = bricks[0]
     bricks.pop(0)
+    hits = 0
+    while len(bricks) > 0:
+        temp = list(bricks)
+        temp.reverse()
+        comp1 = maxbricks(strength, bricks)
+        comp2 = maxbricks(strength, temp)
+        if comp1 > comp2:
+            hits += 1
+            for x in range(comp1):
+                bricks.pop(0)
+        else:
+            hits += 1
+            bricks.reverse()
+            for x in range(comp2):
+                bricks.pop(0)
+    print(hits)
